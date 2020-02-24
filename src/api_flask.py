faf9fb75243a1d75f5ffa144713defcfdd57718a
Mariló Molina Candela <hec.lopezvizcaino@gmail.com>
2020-02-24 20:56:40 +0100

    docker&heroku

--- src/api_flask.py
+++ src/api_flask.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from mRequest import mObject
+from jsonErrorHandler import jsonErrorHandler
 import os
 from dotenv import load_dotenv
 
@@ -8,17 +9,20 @@ load_dotenv()
 app = Flask(__name__)
 
 
+@jsonErrorHandler
 @app.route('/help')
 def start():
     return 'Bienvenid@!! Si tienes dudas de como usar esta API consulta el archivo README.md'
 
 
+@jsonErrorHandler
 @app.route('/user/create/<username>')
 def addUser(username):
     m.createUser(username)
     return f'El usuario {username} ha sido registrado con éxito'
 
 
+@jsonErrorHandler
 @app.route('/chat/create')
 def create_chat():
     name_chat = request.args.get(key='name')
@@ -26,6 +30,7 @@ def create_chat():
     return f'El chat {name_chat} con id {query} ha sido creado con éxito'
 
 
+@jsonErrorHandler
 @app.route('/chat/<chat_id>/adduser')
 def user_to_chat(chat_id):
     addUser = request.args.get(key='user')
@@ -33,6 +38,7 @@ def user_to_chat(chat_id):
     return f'El usuario {addUser} ha sido añadido al chat {query}'
 
 
+@jsonErrorHandler
 @app.route('/chat/<chat_id>/addmessage')
 def message_to_chat(chat_id):
     user = request.args.get(key='user')
@@ -41,18 +47,21 @@ def message_to_chat(chat_id):
     return query
 
 
+@jsonErrorHandler
 @app.route('/chat/<chat_id>/list')
 def all_message(chat_id):
     query = m.alltext_chat(chat_id)
     return query
 
 
+@jsonErrorHandler
 @app.route('/chat/<chat_id>/sentiment')
 def sentiment_chat(chat_id):
     query = m.analyze_chat(chat_id)
     return query
 
 
+@jsonErrorHandler
 @app.route('/chat/recommend')
 def reco():
     docs = request.args.get(key='description')
@@ -61,4 +70,4 @@ def reco():
 
 
 m = mObject()
-app.run("0.0.0.0", os.getenv("port"), debug=True)
+app.run('0.0.0.0', os.getenv("port"), debug=True)
--- src/mRequest.py
+++ src/mRequest.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient
-from jsonErrorHandler import jsonErrorHandler
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity as distance
@@ -19,24 +18,32 @@ class mObject():
         self.col_users = self.mydb['users']
         self.col_chats = self.mydb['chats']
 
-    @jsonErrorHandler
     def user_id(self):
+        '''
+        Revisa si el id del usuario existe o no.
+        Crea un id user único.
+        '''
+        # Lista de id users únicos
         us_id = self.col_users.distinct('_id')
         if us_id == []:
             return 0
         else:
             return max(us_id) + 1
 
-    @jsonErrorHandler
     def user_name(self, username):
+        '''
+        Revisa si el nombre de usuario existe o no.
+        '''
         us_name = self.col_users.distinct('name')
         if username in us_name:
             return 0
         else:
             return 1
 
-    @jsonErrorHandler
     def createUser(self, username):
+        '''
+        Añade un usuario en la colección users.
+        '''
         if self.user_name(username) == 1:
             userInfo = {
                 '_id': self.user_id(),
@@ -47,16 +54,21 @@ class mObject():
         else:
             raise NameError(f'El nombre {username} ya se encuentra registrado')
 
-    @jsonErrorHandler
     def chat_id(self):
+        '''
+        Revisa si el id del chat existe o no.
+        Crea un id chat único.
+        '''
         ch_id = self.col_chats.distinct('_id')
         if ch_id == []:
             return 1000
         else:
             return max(ch_id) + 1
 
-    @jsonErrorHandler
     def createChat(self, name_chat):
+        '''
+        Añade un chat a la colección chats.
+        '''
         new_chat = {
             '_id': self.chat_id(),
             'name': name_chat
@@ -64,62 +76,82 @@ class mObject():
         self.col_chats.insert_one(new_chat)
         return (self.chat_id()) - 1
 
-    @jsonErrorHandler
     def adduser_chat(self, chat_id, adduser):
+        '''
+        Comprueba previamente si el usuario existe en la colección users.
+        Añade usuarios a un chat concreto.
+        '''
+        # Lista de chats id únicos
         ch_id = self.col_chats.distinct('_id')
         if int(chat_id) not in ch_id:
             raise ValueError(f'El chat con id {chat_id} no existe')
-        else:
-            us_name = self.col_users.distinct('name')
+
+        # Lista de users name únicos
+        us_name = self.col_users.distinct('name')
         if adduser not in us_name:
             raise NameError(f'El usuario {adduser} no existe')
-        else:
-            id_adduser = list(self.col_users.find(
-                {'name': adduser}, {'name': 0}))
-            id_n = [v for d in id_adduser for k, v in d.items()][0]
-            self.col_chats.update({'_id': int(chat_id)}, {
-                                  '$push': {'users': int(id_n)}})
+
+        # id de usuario concreto
+        id_adduser = list(self.col_users.find(
+            {'name': adduser}, {'name': 0}))
+        id_n = [v for d in id_adduser for k, v in d.items()][0]
+
+        self.col_chats.update({'_id': int(chat_id)}, {
+            '$push': {'users': int(id_n)}})
         return chat_id
 
-    @jsonErrorHandler
     def addtext_chat(self, chat_id, user, message):
+        '''
+        Comprueba previamente si el chat existe en la colección chats.
+        Añade texto a un chat concreto.
+        '''
+        # Lista de chats id únicos
         ch_id = self.col_chats.distinct('_id')
         if int(chat_id) not in ch_id:
             raise ValueError(f'El chat con id {chat_id} no existe')
-        else:
-            us_name = self.col_users.distinct('name')
+
+        # Lista de users name únicos
+        us_name = self.col_users.distinct('name')
         if user not in us_name:
             raise NameError(f'El usuario{user} no existe')
-        else:
-            ch_us = self.col_chats.distinct('users')
-            id_us = list(self.col_users.find(
-                {'name': user}, {'name': 0}))
-            id_us = [v for d in id_us for k, v in d.items()][0]
-        if id_us not in ch_us:
-            raise NameError(
-                f'El usuario {user} no se encuentra en el chat')
-        else:
-            self.col_chats.update({'_id': int(chat_id)}, {
-                                  '$push': {'content': {'user': int(id_us), 'text': str(message)}}})
+
+        # Lista de usuarios participantes únicos de un chat concreto
+        ch_us = set(self.col_chats.find({'_id': int(chat_id)}, {
+            '_id': 0, 'users': 1}))
+        id_us = list(self.col_users.find({'name': user}, {
+            'name': 0}))
+        id_users = [v for d in id_us for k, v in d.items()][0]
+        if id_users not in ch_us:
+            raise NameError(f'El usuario {user} no se encuentra en el chat')
+
+        self.col_chats.update({'_id': int(chat_id)}, {
+            '$push': {'content': {'user': int(id_us), 'text': str(message)}}})
         return f'Mensaje añadido al chat {chat_id} con éxito'
 
-    @jsonErrorHandler
     def alltext_chat(self, chat_id):
+        '''
+        Muestra todos los mensajes de un chat específico.
+        '''
         messages = list(self.col_chats.find({'_id': int(chat_id)}, {
             '_id': 0, 'content': 1}))
         text = messages[0]
         return text
 
-    @jsonErrorHandler
     def analyze_chat(self, chat_id):
+        '''
+        Realiza un análisis de sentimiento de un chat específico.
+        '''
         text = self.alltext_chat(chat_id)['content']
         t = ' '.join([d['text'] for d in text])
         s = SentimentIntensityAnalyzer()
         result = s.polarity_scores(t)
         return result
 
-    @jsonErrorHandler
     def recommender(self, docs):
+        '''
+        Recomendador de episodios en función de los parámetros descritos por el usuario.
+        '''
+        # Lista de todos los id chats de la colección chats
         c_id = self.col_chats.distinct('_id')
         for _id in c_id:
             all_text = ''
